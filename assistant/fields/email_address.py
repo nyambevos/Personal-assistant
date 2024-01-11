bcc14cdba4b121fbe1c0139e67b6be5dc76daf38
andrii-trebukh <andrew.trebukh@gmail.com>
2024-01-11 23:55:41 +0200

    PEP8 fixed

--- assistant/fields/email_address.py
+++ assistant/fields/email_address.py
@@ -3,10 +3,12 @@
 import re
 from assistant.fields import Field
 
+
 class EmailAddress(Field):
     def is_valid(self, value):
         if re.search(
             r"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$",
-            value):
+            value
+        ):
             return True
         raise ValueError("Incorrect e-mail format")
