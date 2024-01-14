74f27b5988b22e5b763f5576b1f1d2f829f3cb0d
Olha Lialina <145963563+olhalialina@users.noreply.github.com>
2024-01-14 13:13:59 +0200

    feature/data_handler

--- assistant/utils/data_handler.py
+++ assistant/utils/data_handler.py
@@ -1,1 +1,16 @@
 """ Модуль роботи з даними, зчитування/збереження """
+import pickle
+
+def save_data_to_file(filename, data):
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file)
+    print(f'Data saved to {filename} successfully.')
+
+def load_data_from_file(filename):
+    try:
+        with open(filename, 'rb') as file:
+            data = pickle.load(file)
+        print(f'Data loaded from {filename} successfully.')
+        return data
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f'File {filename} not found.') from e
